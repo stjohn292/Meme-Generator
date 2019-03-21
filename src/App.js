8e585c3ada97924dfd0e758c34f534b7ba85d825
Mary McGeary <malicemcgeary@gmail.com>
2019-03-21 14:22:41 -0400

    Created ThumbnailPicker Component and moved code accordingly

--- src/App.js
+++ src/App.js
@@ -1,31 +1,12 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import ThumbnailPicker from "./ThumbnailPicker";
 
 class App extends Component {
   render() {
     return (
-      <div id="wholePage">
-        <div id="ImageChooser">
-          <h1>Choose an Image</h1>
-          <ul>
-            <li>
-              <img src="/Images/Baby.jpg" />
-            </li>
-            <li>
-              <img src="/Images/2.jpg" />
-            </li>
-
-            <li>
-              <img src="/Images/3.jpg" />
-            </li>
-            <li>
-              <img src="/Images/RockRainbow.jpg" />
-            </li>
-          </ul>
-        </div>
-        <div id="Preview" />
-        <div id="Input" />
+      <div>
+        <ThumbnailPicker />
       </div>
     );
   }
---
+++ src/ThumbnailPicker.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+
+class ThumbnailPicker extends Component {
+  render() {
+    return (
+      <div id="wholePage">
+        <div id="ImageChooser">
+          <h1>Choose an Image</h1>
+          <ul>
+            <li>
+              <img src="/Images/Baby.jpg" />
+            </li>
+            <li>
+              <img src="/Images/2.jpg" />
+            </li>
+
+            <li>
+              <img src="/Images/3.jpg" />
+            </li>
+            <li>
+              <img src="/Images/HiDog.jpg" />
+            </li>
+          </ul>
+        </div>
+        <div id="Preview" />
+        <div id="Input" />
+      </div>
+    );
+  }
+}
+
+export default ThumbnailPicker;
