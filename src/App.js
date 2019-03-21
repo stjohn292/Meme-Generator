710c5d3b084b026e667979bbed0169d5947f632f
Mary McGeary <malicemcgeary@gmail.com>
2019-03-21 13:51:38 -0400

    page formatted, images scroll

--- src/App.js
+++ src/App.js
@@ -5,25 +5,28 @@ import "./App.css";
 class App extends Component {
   render() {
     return (
-      <div id="ImageChooser">
-        <h1>Choose an Image</h1>
-        <ul>
-          <li>
-            <img src="/Images/Baby.jpg" />
-          </li>
-          <li>
-            <img src="/Images.2.jpg" />
-          </li>
+      <div id="wholePage">
+        <div id="ImageChooser">
+          <h1>Choose an Image</h1>
+          <ul>
+            <li>
+              <img src="/Images/Baby.jpg" />
+            </li>
+            <li>
+              <img src="/Images/2.jpg" />
+            </li>
 
-          <li>
-            <img src="/Images/Baby.jpg" />
-          </li>
-        </ul>
+            <li>
+              <img src="/Images/3.jpg" />
+            </li>
+            <li>
+              <img src="/Images/RockRainbow.jpg" />
+            </li>
+          </ul>
+        </div>
+        <div id="Preview" />
+        <div id="Input" />
       </div>
-      //   <div id="Preview">
-      //   </div>
-      //   <div id="Input">
-      //   </div>
     );
   }
 }
