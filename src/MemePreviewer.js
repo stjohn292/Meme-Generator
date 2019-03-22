c1cfaf78cb65a8107c4a13be43f901baa1b06923
Mary McGeary <malicemcgeary@gmail.com>
2019-03-22 14:21:59 -0400

    styled previewer box

--- src/MemePreviewer.js
+++ src/MemePreviewer.js
@@ -2,15 +2,14 @@ import React, { Component } from "react";
 
 class MemePreviewer extends Component {
   render() {
-    const { topText, selectedImage , bottomText } = this.props;
-    
+    const { topText, selectedImage, bottomText } = this.props;
+
     return (
       <>
         <div id="Preview">
-          {/* <span /> */}
-          <span>{topText}</span>
-          <img src={selectedImage} />
-          <span>{bottomText}</span>
+          <span id="topText">{topText}</span>
+          <img id="image" src={selectedImage} />
+          <span id="bottomText">{bottomText}</span>
         </div>
       </>
     );
