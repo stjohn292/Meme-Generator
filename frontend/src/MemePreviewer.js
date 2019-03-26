5286d0568a5206bac29a975531d59d0fe8a2eb16
Mo <mshwss@gmail.com>
2019-03-26 13:50:55 -0400

    save button added

--- frontend/src/MemePreviewer.js
+++ frontend/src/MemePreviewer.js
@@ -2,6 +2,26 @@ import React, { Component } from "react";
 import "./MemePreviewer.css";
 
 class MemePreviewer extends Component {
+  // addNew = text => {
+  //   fetch("https://localhost:44374/api/values", {
+  //     method: "POST",
+  //     body: JSON.stringify(text),
+  //     headers: {
+  //       "Content-Type": "application/json"
+  //     }
+  //   })
+  //   .then( res => {
+  //       if (res.ok){
+  //         const newMeme = ([...this.state.image, images] +
+  //         [...this.state.topText, text] +
+  //         [...this.state.bottomText, text]);
+  //         this.setState({ meme: newMeme });
+  //       }    
+  //   })
+  //   .catch(err => {
+  //     console.error(err);
+  //   });  
+  // };
   render() {
     const { topText, selectedImage, bottomText } = this.props;
 
@@ -11,8 +31,13 @@ class MemePreviewer extends Component {
           <span id="topText">{topText}</span>
           <img id="image" src={selectedImage} />
           <span id="bottomText">{bottomText}</span>
+        
+          
+          <input type="meme" value={[ topText, selectedImage, bottomText ]}  onChange={this.onTextChange} />       
+          
+          <button onClick={this.onAdd}>Add</button>
         </div>
-      </>
+      </>   
     );
   }
 }
